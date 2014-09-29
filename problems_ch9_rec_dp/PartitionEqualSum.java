435211801b04def77a683099f6594f3f6a8ec366
ahsanulkarim <ahsanulkarim@vm-0.ahsanulkarim.koding.kd.io>
2014-09-29 07:23:19 +0000

    Partition Equal sum DP solution added

--- problems_ch9_rec_dp/PartitionEqualSum.java
+++ problems_ch9_rec_dp/PartitionEqualSum.java
@@ -8,12 +8,51 @@ public class PartitionEqualSum {
 		int[] data = {4, 3, 1, 2};
 		// O(n lg(n))
 		//Sort.quickSort(data, 0, data.length - 1);
-		boolean isEqual = findPartition(data, data.length);
+		//boolean isEqual = findPartition(data, data.length);
+		boolean isEqual = findPartitionDP(data, data.length);
 		System.out.println("Result: " + isEqual);		
 
 		/*for(int i = 0; i < data.length; i++){
 			System.out.print(data[i] + " ");
 		}*/
+	}
+
+	public static boolean findPartitionDP(int[] a, int n) {
+		// sum
+                int sum = 0;
+                for(int i = 0; i < n; i++){
+                        sum += a[i];
+                }
+	        if((sum%2) != 0) return false;
+
+		boolean part[][] = new boolean[sum/2 + 1][n + 1];
+		for(int i = 0; i <= n; i++){
+			part[0][i] = true;
+		}
+
+		for(int i = 1; i <= sum/2; i++) {
+			part[i][0] = false;
+		}
+
+		for(int i = 1; i <= sum/2; i++){
+			for(int j = 1; j<= n; j++){
+				part[i][j] = part[i][j-1];
+				if(i >= a[j-1]) {
+					part[i][j] = part[i][j] || part[i - a[j-1]][j-1];
+				}
+				//System.out.print( part[i][j]+"\t");
+			}
+			//System.out.println();
+		}
+		for(int i = 0; i <= sum/2; i++){
+                        for(int j = 0; j<= n; j++){
+				System.out.print( part[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		
+		return part[sum/2][n];
+
 	}
 
 	public static boolean findPartition(int[] a, int n) {
